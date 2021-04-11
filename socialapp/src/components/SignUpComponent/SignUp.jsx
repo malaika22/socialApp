84418455982334cdcc4a24463cce531bfd8e5977
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-11 15:40:15 +0500

    Create user in database on signing up

--- socialapp/src/components/SignUpComponent/SignUp.jsx
+++ socialapp/src/components/SignUpComponent/SignUp.jsx
@@ -1,18 +1,74 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import firebase from '../../fbConfig'
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
-import {Form, Input} from 'antd'
+import {Form, Input, Button, DatePicker, Select} from 'antd'
+import {useHistory} from 'react-router-dom'
+import UserContext from '../../contexts/UserContext'
 import './styles.scss'
 
 const SignUp = () =>{
-
     const [signUpUser, setSignUpUser] = useState({
         username: '',
         uid: '',
-        email: ''
+        email: '',
+        bio: '',
+        gender: '',
+        password: '',
+        dateOfBirth: '',
+        confirmPassword: ''
     })
-    const finishHandler = () =>{
-        if(signUpUser.email && signUpUser.username){
+    const {Option} = Select;
+   const {TextArea} = Input
+  let history = useHistory() 
+    const db = firebase.firestore()
 
+      const createUser = (res) =>{
+        console.log(res)
+        console.log(res.uid)
+          db.collection('users').add({
+            uid: res.uid,
+            username: signUpUser.username,
+            email: signUpUser.email,
+            bio: signUpUser.bio,
+            dateOfBirth: signUpUser.dateOfBirth,
+            gender: signUpUser.gender
+          })
+      }
+
+    const changeHandler = (e) =>{
+      setSignUpUser({
+        ...signUpUser,
+          [e.target.name] : e.target.value
+      })
+    }
+
+    const handleDateChange = (date, dateString) =>{
+      setSignUpUser({
+        ...signUpUser,
+        dateOfBirth: dateString
+      })
+    }
+
+    const handleGenderChange = (value) =>{
+      setSignUpUser({
+        ...signUpUser,
+        gender: value
+      })
+    }
+    const finishHandler = () =>{
+      console.log(signUpUser)
+        if(signUpUser.email && signUpUser.username){
+            firebase.auth().createUserWithEmailAndPassword(signUpUser.email,signUpUser.password)
+            .then(res=>{
+              console.log("Successfully signed in")
+              // Add user to database
+              console.log(res)
+              createUser(res.user)
+              history.push('/home')
+
+            }).catch(err=>{
+              console.log('Error signing up' , err)
+            })
         }
     }
     return(
@@ -27,7 +83,10 @@ const SignUp = () =>{
                 >
                     <Input name="username" 
                     placeholder="Enter your username"
-                    type="text"/>
+                    type="text"
+                    value={signUpUser.username}
+                    onChange={changeHandler}
+                    />
                 </Form.Item>
 
                 <Form.Item
@@ -37,7 +96,8 @@ const SignUp = () =>{
                 <Input name="email" 
                     placeholder="Enter your email"
                     type="email"
-                    
+                    value={signUpUser.email}
+                    onChange={changeHandler}
                     />
                 </Form.Item>
                 
@@ -47,13 +107,17 @@ const SignUp = () =>{
                 { required: true, message: "Password can't be an empty field" },
               ]}
               className="password-field formLabel"
+              label="Password"
             >
               <Input.Password
+                name="password"
                 placeholder="Your Password"
                 iconRender={(visible) =>
                   visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
                 }
                 className="password-input input-field"
+                value={signUpUser.password}
+                onChange={changeHandler}
               />
             </Form.Item>
 
@@ -61,6 +125,7 @@ const SignUp = () =>{
               name="confirmPassword"
               dependencies={["password"]}
               hasFeedback
+              label="Confirm password"
               className="password-field formLabel"
               rules={[
                 {
@@ -86,9 +151,45 @@ const SignUp = () =>{
                 iconRender={(visible) =>
                   visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
                 }
+                name="confirmPassword"
+                value={signUpUser.confirmPassword}
                 className="password-input input-field"
+                onChange={changeHandler}
               />
             </Form.Item>
+
+          <Form.Item
+          label="Date of Birth">
+            <DatePicker onChange={handleDateChange}/>
+          </Form.Item>
+
+          <Form.Item>
+            <Select
+            onChange={handleGenderChange}
+            >
+                <Option value="female">Female</Option>
+                <Option value="male">Male</Option>
+                <Option value="non-binary">Non-binary</Option>
+                <Option value="intersex">Intersex</Option>
+                <Option value="other">Other</Option>
+            </Select>
+
+          </Form.Item>
+
+          <Form.Item>
+              <TextArea rows={4} 
+              name="bio"
+              value={signUpUser.bio}
+              onChange={changeHandler}
+              />
+          </Form.Item> 
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" key="submit" onClick={finishHandler}>
+              Submit
+            </Button>
+          </Form.Item>
+
 
             </Form>
         </div>
