69dfb0daa738764a6f8d7f6200967362c40a72e2
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-11 18:22:20 +0500

    Implemented logout Users in nav header

--- socialapp/src/components/NavHeader/NavHeader.jsx
+++ socialapp/src/components/NavHeader/NavHeader.jsx
@@ -7,12 +7,22 @@ import './styles.scss'
 
 const NavHeader = () =>{
 
+    const handleLogout = () =>{
+        firebase.auth().signOut()
+        .then(res=>{
+            console.log("Successfully logged in")
+        })
+        .catch(err => {
+            console.log("error logging out" , err)
+        })
+    }
     return (
         <div>
             <Link to="/home">Home</Link>
             <Link to="/chat">Chat</Link>
             <Link to="/quotes">Quotes</Link>
             <Link to="/music">Music</Link>
+            <Button type="primary" onClick={handleLogout}>Logout</Button>
         </div>
     )
 }
