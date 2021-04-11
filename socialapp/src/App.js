2aba82aa6aa45674385dbc9ca76349f4ed1410b1
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-11 12:43:30 +0500

    Added routes

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -1,15 +1,18 @@
 import Login from './components/LoginComponent/Login'
 import SignUp from './components/SignUpComponent/SignUp'
+import Home from './components/Home/Home'
 import './App.css';
 import { Route, Switch } from 'react-router';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={SignUp}/>
-    </Switch>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp}/>
+        <Route path="/home" component={Home} />
+      </Switch>
   );
 }
 
 export default App;
+ 
