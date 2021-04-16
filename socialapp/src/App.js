4bdc28a71d62424dcdb51fc5870f9d7dd46e2ca9
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-17 02:02:20 +0500

    Added routes

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -1,15 +1,21 @@
+import React from 'react'
+import { Route, Switch } from 'react-router';
 import Login from './components/LoginComponent/Login'
 import SignUp from './components/SignUpComponent/SignUp'
 import Home from './components/Home/Home'
+import UserProfile from './components/UserProfile/UserProfile'
 import './App.css';
-import { Route, Switch } from 'react-router';
+
 
 function App() {
   return (
       <Switch>
+         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp}/>
-        <Route path="/home" component={Home} />
+        <Route exact path="/signup" component={SignUp}/>
+        <Route path="/user/:userId" render={({history, match})=>
+         <UserProfile
+         userId={match.params.userId}/>} />
       </Switch>
   );
 }
