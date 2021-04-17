d1156e6004983dee627772e26660b7d19fe96058
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-17 23:42:35 +0500

    Added Chat routing

--- socialapp/src/App.js
+++ socialapp/src/App.js
@@ -4,19 +4,29 @@ import Login from './components/LoginComponent/Login'
 import SignUp from './components/SignUpComponent/SignUp'
 import Home from './components/Home/Home'
 import UserProfile from './components/UserProfile/UserProfile'
+import ChatSection from './components/ChatComponent/ChatSection'
 import './App.css';
+import NavHeader from './components/NavHeader/NavHeader';
 
 
 function App() {
   return (
+    <>
+    
       <Switch>
-         <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
+         <Route path="/login" component={Login} />
         <Route exact path="/signup" component={SignUp}/>
+         <NavHeader>
+         <Route exact path="/" component={Home} />
         <Route path="/user/:userId" render={({history, match})=>
          <UserProfile
          userId={match.params.userId}/>} />
+         <Route path="/chat" component={ChatSection}/>
+         </NavHeader>
+
       </Switch>
+
+      </>
   );
 }
 
