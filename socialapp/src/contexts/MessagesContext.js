62c2b5c350b9eff41c3e6d1f8adea9605fed7ac5
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-19 03:33:17 +0500

    Added handleSubmitMessage function

--- socialapp/src/contexts/MessagesContext.js
+++ socialapp/src/contexts/MessagesContext.js
@@ -1,12 +1,48 @@
-import React, { createContext } from 'react'
-
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import firebase from '../fbConfig'
+import { UserContext } from './UserContext'
 
 export const MessagesContext = createContext()
 
 export const MessagesContextProvider = ({children}) => {
+    const db = firebase.firestore()
+    const [messages , setMessages] = useState([])
+    const {currentUser} = useContext(UserContext)
+
+
+    useEffect(()=>{
+            db.collection("messages").onSnapshot(snapshot =>{
+                const dataArr = [];
+                console.log("in message useEffect")
+                snapshot.forEach(doc =>{
+                    dataArr.push(doc.data())
+                })
+
+                setMessages(dataArr)
+            })
+            
+    },[])
+
+    const handleSubmitMessage = (msg) =>{
+        console.log("in send message")
+        db.collection("messages").add({
+            msg : msg,
+            user_id : currentUser.uid,
+            author: currentUser.username,
+            timeStamp : new Date()
+        })
+    }
+
+
+
+
     return(
     <MessagesContext.Provider
-    value={{}}>
+    value={{
+        messages : messages,
+        //handleSubmitMsg: handleSubmitMsg
+        handleSubmitMessage: handleSubmitMessage
+    }}>
         {children}
     </MessagesContext.Provider>
     )
