76c5451d7ae98c7f5c6577c2ce9f5537591be994
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-20 16:27:41 +0500

    Styled Login Component

--- socialapp/src/components/LoginComponent/Login.jsx
+++ socialapp/src/components/LoginComponent/Login.jsx
@@ -1,13 +1,15 @@
 import React, {useState, useContext} from 'react'
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
-import {Form, Input, Button} from 'antd'
+import {Form, Input, Button, Layout} from 'antd'
 import {Link, Redirect, useHistory} from 'react-router-dom'
 import {UserContext} from '../../contexts/UserContext'
 import firebase from '../../fbConfig'
+import signUp from '../../assests/signUp.png'
 import './styles.scss'
 
 
 const Login = () =>{
+  const {Content} = Layout
   const history = useHistory()
   const {currentUser} = useContext(UserContext)
   console.log(currentUser)
@@ -57,51 +59,73 @@ const Login = () =>{
     console.log('login')
 
 
-    if(currentUser) {
+    /*if(currentUser) {
       console.log("in login")
       console.log(currentUser)
-      return <Redirect to="/" />}
+      return <Redirect to="/" />} */
     return(
-        <div>
-      <Form
-      name="login-form"
-      onFinish={finishHandler}
-      >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your email!' }]}
-      >
-        <Input onChange={changeHandler}
-        placeholder="Enter your email here"
-        type="email"
-        name="email" />
-      </Form.Item>
+            <div className="login-main-div">
+              <div className="login-left-screen">
+                    <div className="left-screen-content-div">
+                        <div className="left-screen-content">
+                          Your new social media platform with stuff that actually matters
+                        </div>
+                        <div className="left-screen-img">
+                          <img src={signUp} alt="Sign up"/>
+                        </div> 
+                    </div>
+              </div>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password
-                placeholder="Enter your password here"
-                iconRender={(visible) =>
-                  visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-                }
-                className="password-input input-field"
-                onChange={changeHandler}
-                name="password"
-        />
-      </Form.Item>
-                {credentials.error&& <p>{credentials.error}</p>}
-                <p>Not registered? Signup <Link to="/signup"><span>here</span></Link></p>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" key="submit" onClick={finishHandler}>
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-        </div>
+              <div className="login-right-screen">
+                <Form
+                name="login-form"
+                onFinish={finishHandler}
+                className="login-form"
+                >
+                  <div className="login-heading">
+                        Sign In
+                  </div>
+                  <Form.Item
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your email!' }]}
+                    className="login-label"
+                  >
+                    <Input onChange={changeHandler}
+                    placeholder="Enter your email here"
+                    type="email"
+                    name="email" 
+                    className="login-input input-field"
+                    />
+                  </Form.Item>
+
+                  <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                    className="login-label"
+                  >
+                    <Input.Password
+                            placeholder="Enter your password here"
+                            iconRender={(visible) =>
+                              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+                            }
+                            className="password-input input-field"
+                            onChange={changeHandler}
+                            name="password"
+                    />
+                  </Form.Item>
+                        {credentials.error&& <p>{credentials.error}</p>}
+                        <p>Not registered? Signup <Link to="/signup"><span style={{color: '#f97b47'}}>here</span></Link></p>
+                    <Form.Item className="button-label">
+                      <Button type="primary" htmlType="submit" key="submit" onClick={finishHandler}
+                      className="login-button"
+                      >
+                        Submit
+                      </Button> 
+                    </Form.Item>
+                  </Form>
+              </div>
+
+            </div>
     )
 }
 
