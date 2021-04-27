b384112432c89ec21cb9f30f8442c5bde506bcd9
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-28 00:32:32 +0500

    Update post likes

--- socialapp/src/components/PostsContainer/Post/Post.jsx
+++ socialapp/src/components/PostsContainer/Post/Post.jsx
@@ -1,18 +1,88 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, createElement, useEffect, useRef } from 'react'
 import './styles.scss'
 import {Avatar} from 'antd'
+import {LikeFilled, LikeOutlined} from '@ant-design/icons'
 import {UserOutlined} from '@ant-design/icons'
 import moment from 'moment'
 import { Link, useParams } from 'react-router-dom'
 import { UserContext } from '../../../contexts/UserContext'
+import { PostsContext } from '../../../contexts/PostsContext'
 
-const Post = ({postDesc}) =>{
+const Post = ({postDesc}) => {
     const {currentUser} = useContext(UserContext)
+    const {likePost, dislikePost, updatePost,alreadyLike, posts} = useContext(PostsContext)
     const {userId} = useParams()
-    console.log(moment(postDesc.createdAt.toDate()).fromNow())
-    console.log(postDesc)
-    console.log(currentUser)
-    console.log(userId)
+ 
+    console.log(posts)
+
+    const changeLike = (postDesc) =>{
+        console.log(postDesc.liked)
+      const updatedPosts =  posts.map(post => 
+
+        /* 
+        Like Functionality firebase
+
+        When user likes post 
+        add userId in posts likedBy field
+        increment post like by one
+        check if the likedBy id === user logged in if true => likedFilledIcon if false likedIcon
+
+        if the like Action is dislike and the user is in the LikedBy field 
+        remove user from likedBy field
+        decrement likes 
+
+        */
+
+       /* {
+            if(post.id===postDesc.id) {
+                if(post.liked ===undefined || post.liked===false) {
+                    console.log('in like')
+                    console.log(post)
+                    //likePost(post.id)
+                   return {
+                        ...post , 
+                        liked : true,
+                        likes: post.likes + 1
+                    }
+
+                } else {
+                    //console.log(post.liked)
+                    console.log('in dislike')
+                    console.log(post)
+                    //dislikePost(post.id)
+                    return {
+                        ...post,
+                        liked : false,
+                        likes : post.likes -1
+                    }
+                }
+                
+            } else {
+                return post
+            }
+        }    */
+
+        // Changing likes using Global State =>> needs to add likes through database
+
+        post.id === postDesc.id ? 
+         post.liked === undefined || post.liked === false ?
+         //likePost(post.id)
+         {
+            ...post ,
+            likes : post.likes + 1,
+            liked : true
+        } : {
+            ...post , 
+            likes : post.likes -1,
+            liked: false
+        } : post 
+        )
+
+        updatePost(updatedPosts)
+    
+    }
+   
+ 
     return (
         <div>   
             <div>
@@ -25,7 +95,8 @@ const Post = ({postDesc}) =>{
             </div>
             
             <div>{postDesc.post}</div> 
-            <div>{postDesc.likes}</div> <span>{moment(postDesc.createdAt.toDate()).fromNow()}</span>
+            <span onClick={()=>changeLike(postDesc)}> {createElement(postDesc.liked  ? LikeFilled : LikeOutlined)} </span>  <span> {postDesc.likes}   </span>
+            <span>{moment(postDesc.createdAt.toDate()).fromNow()}</span>
         </div>
     )
 }
--- socialapp/src/components/PostsContainer/PostsList/PostsList.jsx
+++ socialapp/src/components/PostsContainer/PostsList/PostsList.jsx
@@ -1,14 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Post from '../Post/Post'
 import { PostsContext } from '../../../contexts/PostsContext'
 import './styles.scss'
 
 const PostList = () => {
-    const {posts} = useContext(PostsContext)
-    console.log(posts)
+    const {posts} = useContext(PostsContext)   
     return (
         <div>
-            {posts.map(post => <Post postDesc ={post}/>)}
+            {posts.map(post => <Post postDesc ={post} />)}
         </div>
     )
 }
