0f7f05dcdfbdf2b911ecf5fefcfcd70f56ec3f24
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-29 04:02:43 +0500

    Styled Follow Suggest Card

--- socialapp/src/components/Home/FollowSuggestionContainer/FollowSuggestCard/FollowSuggestCard.jsx
+++ socialapp/src/components/Home/FollowSuggestionContainer/FollowSuggestCard/FollowSuggestCard.jsx
@@ -6,16 +6,16 @@ import './styles.scss'
 const FollowSuggestCard = () =>{
     return(
         <div className="follow-card-main-div">
-            <div class Name="follow-card-content-div">
+            <div className="follow-card-content-div">
                 <div className="avatar-div">
-                    <Avatar icon={<UserOutlined/>}/>
+                    <Avatar size={40} icon={<UserOutlined/>} className="follow-avatar"/>
                 </div>
                 <div className="user-content">
                     <div className="user-name">Malaika Afridi</div>
                     <div className="user-bio">Neque porro quisquam est qui dolorem ipsum quia dolor sit ... </div>
                 </div>
                 <div className="follow-button-div">
-                    <UserAddOutlined/>
+                    <UserAddOutlined className="add-user-icon"/>
                 </div>
             </div>
         </div>
